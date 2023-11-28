87870cc546df55d2298446f3bc54e6afb690248b
whyrulike <105625915+whyrulike@users.noreply.github.com>
2023-11-28 16:16:14 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-
+# install python env
 sudo apt update
-sudo apt install python3-pip
-
-# 安装 prometheus_client 库
+sudo apt install python3-pip -y
 pip3 install prometheus_client
-# install loguru
 pip3 install loguru
 pip3 --version
 
+# install dependent
 sudo apt update && sudo apt install snapd -y && sudo apt-get install git -y && sudo apt-get install gcc -y  && sudo apt-get install make -y
 sudo snap install lz4
 
-
+# install go
 sudo rm -rvf /usr/local/go/
 wget https://golang.org/dl/go1.21.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.21.1.linux-amd64.tar.gz
